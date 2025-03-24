a64bc6ced09b631ba78fa6a66baca7d9afd5f947
Mohd noman Qadri <60312089+nomanqadri34@users.noreply.github.com>
2025-03-25 00:31:45 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,15 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css"; // Import Toast CSS
+
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Services from "./pages/Services";
-
 import WorkExperience from "./pages/WorkExperience";
 import Contact from "./pages/Contact";
-
 import TeamMembers from "./pages/team";
 import Login from "./pages/Login";
 import AdminDashboard from "./pages/AdminDashboard";
@@ -28,7 +29,6 @@ function App() {
           <Route path="/home" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/services" element={<Services />} />
-          
           <Route path="/work-experience" element={<WorkExperience />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/team-member" element={<TeamMembers />} />
@@ -49,9 +49,14 @@ function App() {
           />
         </Routes>
       </main>
+      
+      {/* Toast Notification Container */}
+      <ToastContainer position="top-right" autoClose={3000} />
+      
       <Footer />
     </Router>
   );
 }
 
 export default App;
+
